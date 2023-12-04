a5989137e6ee0dcaa9dc28e3643f94423917db8a
rezha4 <rezha.bahari@gmail.com>
2023-12-04 09:51:10 +0700

    refactoring time logic

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
+import bodyParser, { json } from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGO);
+}
+
+const leaderBoardSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  time: { type: Number, default: timeValue },
+  date: { type: Date, default: Date.now },
+});
+
+const LeaderBoard = mongoose.model("leaderBoard", leaderBoardSchema);
 
 app.use(cors());
 app.use(bodyParser.json());
 
 let correctAnswers = [];
 
-let timeValue = 0;
-let timerInterval;
+app.get("/leaderboard", async (req, res) => {
+  try {
+    const leaderBoardData = await LeaderBoard.find({});
+    res.status(200).json(leaderBoardData);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timeValue++;
-  }, 1000);
-}
-
-function stopTimer() {
-  timeValue = 0;
-  clearInterval(timerInterval);
-}
+app.post("/form", async (req, res) => {
+  const name = req.body.name;
+  try {
+    const newScore = new LeaderBoard({
+      name: name,
+      time: timeValue,
+    });
+    await newScore.save();
+    res.status(200).json({ message: "Saved" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "error" });
+  }
+});
 
 app.get("/time", (req, res) => {
   res.json({ time: timeValue });
@@ -40,9 +64,10 @@ app.post("/answers", (req, res) => {
 
 app.get("/game-completed", (req, res) => {
   if (correctAnswers.length >= 3) {
-    res.status(200).json({ completed: true, message: "gamedone" });
     correctAnswers = [];
     stopTimer();
+    timeValue = 0;
+    res.status(200).json({ completed: true, message: "gamedone" });
   } else {
     res.status(200).json({ completed: false, message: "not yeat" });
   }
